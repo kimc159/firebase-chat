e78a11676687ad79da52a06df723e8a16d8c38f4
이주열 <ljykimc159@gmail.com>
2018-04-09 16:11:14 +0900

    메인 페이지 button 아래로 수정

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <v-app>
-    <v-toolbar dark color="teal lighten-3" v-if="userIsAuth">
-      <v-toolbar-items>
-        <v-btn flat to="/chatlist" @click="userIsAuth = true">
-          <v-icon class="mr-2">message</v-icon>
-          채팅
-        </v-btn>
-        <v-btn flat to="/setting" @click="userIsAuth = true">
-          <v-icon class="mr-2">settings</v-icon>
-          설정
-        </v-btn>
-      </v-toolbar-items>
+    <v-toolbar class="text-xs-center" dark color="teal lighten-3" v-if="user">
+      <v-toolbar-title class="white--text">firebase chat</v-toolbar-title>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
     </v-content>
+    <v-tabs icons-and-text centered dark color="teal lighten-3" v-if="user">
+      <v-tabs-slider color="yellow"></v-tabs-slider>
+      <v-tab href="#tab-1" to="/userlist" @click="userIsAuth = true">
+        <v-icon class="mr-2">person</v-icon>
+        친구
+      </v-tab>
+      <v-tab href="#tab-2" to="/chatlist" @click="userIsAuth = true">
+        <v-icon class="mr-2">message</v-icon>
+        채팅
+      </v-tab>
+      <v-tab href="#tab-3" to="/setting" @click="userIsAuth = true">
+        <v-icon class="mr-2">settings</v-icon>
+        설정
+      </v-tab>
+    </v-tabs>
   </v-app>
 </template>
 
 <script>
-import * as firebase from 'firebase'
 
 export default {
   data () {
@@ -28,19 +33,26 @@ export default {
     }
   },
   name: 'App',
-  mounted () {
-  },
   methods: {
+  },
+  computed: {
+    user () {
+      return this.$store.getters.user
+    }
   }
 }
 </script>
 
 <style lang="scss">
-  .toolbar__items{
+  .toolbar__content .toolbar__title {
+    margin: 0 auto;
+  }
+  .content {
     width: 100%;
-    a{
-      width: 50%;
-    }
+    height: auto;
+  }
+  .tabs .tabs__div {
+    width: 50%;
   }
 </style>
 
